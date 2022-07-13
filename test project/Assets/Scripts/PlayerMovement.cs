cb358b1d10f4f0187501f29aa144bebb843c71f6
Ki-jana Garvey <77340631+Only-Jube@users.noreply.github.com>
2022-07-13 16:48:23 -0400

    Vertical and Horizontal movement
    Added some generic movement functions. There are a few placeholder names such as "gameObject" in reference to the actual game object.

--- test project/Assets/Scripts/PlayerMovement.cs
+++ test project/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+private CharacterController controller;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = gameObject.AddComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+        //Moves player left or right if the move input not a jump. In other words if it's "left" or "right"
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
         
+        //Moves player vertically. In other words, a jump
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
